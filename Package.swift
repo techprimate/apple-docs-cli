c9e83e9f568aae452ae9fba394df2765fd651e47
Philip Niedertscheider <phil@techprimate.com>
2026-09-08 14:07:05 +0200

    build: Configure Swift package

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.3
+
+import PackageDescription
+
+let package = Package(
+    name: "apple-docs-cli",
+    platforms: [.macOS(.v13)],
+    products: [
+        .executable(name: "apple-docs", targets: ["CLI"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.8.2")
+    ],
+    targets: [
+        .executableTarget(
+            name: "CLI",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]),
+        .testTarget(name: "CLITests", dependencies: ["CLI"]),
+    ],
+    swiftLanguageModes: [.v6]
+)
